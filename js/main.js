d244368a8e91b261fe9c57cbad124a69175156b6
Valmir Almeida <almeidavalmir76@gmail.com>
2022-05-10 02:23:20 -0300

    template prototipo inicial home page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Menu mobile
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navigation-bar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+window.addEventListener("scroll", () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+});
